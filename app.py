ca88df757f4547f5849037c8e79f295c77990d22
godot63 <lcalmbach@gmail.com>
2021-08-11 07:08:10 +0200

    Add labels to columns in sql statement

--- app.py
+++ app.py
@@ -39,9 +39,9 @@ def execute_query(query: str, cn) -> pd.DataFrame:
 
 
 def get_tables(cat):
-    sql = f"select id, table_name from stat_table where category_id = {cat} order by category_id, sort_key"
+    sql = f"select id, name from stat_table where category_id = {cat} order by category_id, sort_key"
     df = execute_query(sql,conn)
-    result = dict(zip( list(df['id']), list(df['table_name']) ))
+    result = dict(zip( list(df['id']), list(df['name']) ))
     return result
 
 
@@ -61,16 +61,19 @@ def get_table_name(table_id):
 def get_group_fields(column_id_list):
     csv_list = result = ','.join(map(str,column_id_list))
 
-    sql = f"select name from stat_table_column where id in ({csv_list}) order by sort_key"
+    sql = f"select * from stat_table_column where id in ({csv_list}) order by sort_key"
     df = execute_query(sql,conn)
-    result = ','.join(df['name'])
-    return result
+    df['col_expression'] = df['name'] + ' as \'' + df['label'] + '\''
+    result = ",".join(list(df['col_expression']))
+    result_no_label = ",".join(list(df['name']))
+    return result, result_no_label
 
 
-def get_data(group_field_ids, sum_field, table_id):
+def get_data(group_field_ids, sum_fields, table_id):
     table_name = get_table_name(table_id)
-    group_fields = get_group_fields(group_field_ids)
-    sql = f"select {group_fields}, {sum_field} from {table_name} group by {group_fields} order by {group_fields}"
+    group_fields, group_fields_no_label = get_group_fields(group_field_ids)
+    sql = f"select {group_fields}, {sum_fields} from {table_name} group by {group_fields_no_label} order by {group_fields_no_label}"
+    # st.write(sql)
     df = execute_query(sql,conn)
     return df
 
@@ -83,11 +86,10 @@ def get_fields(table_id):
 
 
 def get_sum_fields(table_id):
-    sql = f"select name from stat_table_column where stat_table_id = {table_id} and col_type_id = 3 order by sort_key"
+    sql = f"select name,label from stat_table_column where stat_table_id = {table_id} and col_type_id = 3 order by sort_key"
     df = execute_query(sql,conn)
-    df['name'] = df['name'] 
-    lst = [f"sum({x}) as {x}" for x in list(df['name'])]
-    result = ",".join(lst)
+    df['col_expression'] = "sum(" + df['name'] + ') as \'' + df['label'] + '\''
+    result = ",".join(list(df['col_expression']))
     return result
 
 
@@ -114,19 +116,26 @@ def get_plot_options():
 def get_chart(df, plot_type, plot_options):
     if plot_type ==  'bar':
         chart = alt.Chart(df).mark_bar().encode(
-            x='jahr:N',
-            y='anzahl:Q'
+            x='Jahr:N',
+            y='Anzahl:Q'
         )
-    return chart.properties(width = 800)
+    return chart.properties(width = 400)
 
 
-def get_metadata():
+def get_metadata(df):
     sql = f"select * from stat_table where id =  {settings['table']}"
     df_table = execute_query(sql,conn)
     sql = f"select * from stat_table_column where stat_table_id =  {settings['table']}"
     df_columns = execute_query(sql,conn)
-    table_expression = f"**Beschreibung**: {df_table.iloc[0]['description']}, **Datenquelle**: {df_table.iloc[0]['data_source']}"
-    
+    column_expression = '<br>**Spalten**:<br>'
+    for index, row in df_columns.iterrows():
+        column_expression += f"{row['label']}: {row['description']}<br>"
+    table_expression = f"**Beschreibung**: {df_table.iloc[0]['description']}<br>**Datenquelle**: {df_table.iloc[0]['data_source']}"
+    if len( df_columns[df_columns['name'] == 'jahr']) > 0:
+        min= df['Jahr'].min()
+        max= df['Jahr'].max()
+        table_expression += f"<br>**Jahre von/bis**: {min} - {max}"
+    table_expression += column_expression
     return table_expression
 
 def main():
@@ -139,19 +148,18 @@ def main():
     
     action = st.selectbox('Zeige', ['Tabelle', 'Grafik', 'Metadaten'])
 
+    df = get_data(settings['group_columns'],  settings['sumfields'], settings['table'])  
     if action.lower() == 'tabelle':
-        df = get_data(settings['group_columns'],  settings['sumfields'], settings['table'])  
         if len(df)>0:
             AgGrid((df))
         else:
             st.markdown('keine Daten gefunden')
     elif action.lower() == 'grafik':
-        df = get_data(settings['group_columns'],  settings['sumfields'], settings['table'])  
         plot_type, plot_options = get_plot_options()
         chart = get_chart(df, plot_type, plot_options)
         st.altair_chart(chart)
     elif action.lower() == 'metadaten':
-        table_expression = get_metadata()
+        table_expression = get_metadata(df)
         st.markdown(table_expression,unsafe_allow_html=True)
 
 if __name__ == "__main__":
